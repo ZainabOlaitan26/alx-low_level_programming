8b0b763dbc02d00c15d374a4187c01fb5422b1d6
ZainabOlaitan26 <zhaynabmonisola@gmail.com>
2022-03-15 05:43:02 -0700

    functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
-#include "putchar.h"
+#include "main.h"
 
 /**
- *  * main - Prints _putchar as a message.
+ *  * main - Prints putchar as a message.
  *   *
  *    * Return: Always 0 (Success)
  */
