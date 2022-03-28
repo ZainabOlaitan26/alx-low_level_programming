e14d27959bb0f8fb909a7db83df6c3cda88a8e67
ZainabOlaitan26 <zhaynabmonisola@gmail.com>
2022-03-28 06:30:51 -0700

    Even more Pointers

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- *  * main - check the code for Holberton School students.
- *   *
- *    * Return: Always 0.
+ * main - check the code
+ *  *
+ *   * Return: Always 0.
  */
-int main(void)
-{
-	char *s = "wworld of this";
-	char *f = "world";
-	char *t;
+ int main(void)
+ {
+     char *s = "hello, world";
+     char *f = "world";
+     char *t;
 
-	t = _strstr(s, f);
-	printf("%s\n", t);
-	return (0);
-}
+     t = _strstr(s, f);
+     printf("%s\n", t);
+     return (0);
+ }
