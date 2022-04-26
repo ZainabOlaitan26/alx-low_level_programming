3357b03a13f0362e682f8db6b43329a413766afe
ZainabOlaitan26 <zhaynabmonisola@gmail.com>
2022-04-26 00:27:29 -0700

    more singly lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,12 +5,12 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for ALX low level programming  project
+ *  * struct listint_s - singly linked list
+ *   * @n: integer
+ *    * @next: points to the next node
+ *     *
+ *      * Description: singly linked list node structure
+ *       * for ALX low level programming  project
  */
 typedef struct listint_s
 {
@@ -19,11 +19,11 @@ typedef struct listint_s
 } listint_t;
 
 /**
- * struct listp_s - singly linked list
- * @p: pointers of nodes
- * @next: points to the next node
- *
- * Description: singly linked list of pointers
+ *  * struct listp_s - singly linked list
+ *   * @p: pointers of nodes
+ *    * @next: points to the next node
+ *     *
+ *      * Description: singly linked list of pointers
  */
 typedef struct listp_s
 {
