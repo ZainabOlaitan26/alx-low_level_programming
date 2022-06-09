54949217b4fdce8c845461d65b99ac8dbdbb5505
ZainabOlaitan26 <zhaynabmonisola@gmail.com>
2022-06-09 08:35:45 -0700

    lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- *  * insert_dnodeint_at_index - inserts a new node at
- *   * a given position
- *    *
- *     * @h: head of the list
- *      * @idx: index of the new node
- *       * @n: value of the new node
- *        * Return: the address of the new node, or NULL if it failed
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
+ *
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
@@ -31,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			{
 				if (head->next == NULL)
 					new = add_dnodeint_end(h, n);
-				else	
+				else
 				{
 					new = malloc(sizeof(dlistint_t));
 					if (new != NULL)
@@ -43,8 +43,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 						head->next = new;
 					}
 				}
-				break;										
-			}		
+				break;
+			}
 			head = head->next;
 			i++;
 		}
