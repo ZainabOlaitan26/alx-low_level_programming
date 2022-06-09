f74a4cc30b7b6c25498c48c79c84d8eafb52a2d6
ZainabOlaitan26 <zhaynabmonisola@gmail.com>
2022-06-09 07:32:08 -0700

    lists

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -3,7 +3,7 @@
 /**
  *  * delete_dnodeint_at_index - deletes the node at index of a
  *   * dlistint_t linked list
- *    *
+ *
  *     * @head: head of the list
  *      * @index: index of the new node
  *       * Return: 1 if it succeeded, -1 if it failed
@@ -35,11 +35,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			else
 			{
 				h2->next = h1->next;
-				
+
 				if (h1->next != NULL)
 					h1->next->prev = h2;
 			}
-			
+
 			free(h1);
 			return (1);
 		}
