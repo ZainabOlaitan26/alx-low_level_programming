5ee27b301899e5c1a4690b68a7068b9c956eceb4
ZainabOlaitan26 <zhaynabmonisola@gmail.com>
2022-06-16 06:36:47 -0700

    dynamic

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
-#!/bin/bash#!/bin/bash
+#!/bin/bash
 wget -P /tmp https://github.com/baccrie/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
 export LD_PRELOAD=/tmp/libmask.so
